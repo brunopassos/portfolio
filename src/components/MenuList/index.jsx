913d0bbb7a829adff7e29a91620a1240f095b3fe
Bruno <brunopassos_bp@hotmail.com>
2022-07-26 21:27:34 -0300

    feat/ adding some contents to each list item

--- src/components/MenuList/index.jsx
+++ src/components/MenuList/index.jsx
@@ -5,21 +5,41 @@ import {
   StyledMenuContainer,
 } from "./styles";
 
-import { AiOutlineInstagram, AiFillGithub } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
+import { useState } from "react";
+
+import { AiFillGithub } from "react-icons/ai";
 
 function MenuList({ isOpen }) {
-  function handleGithubClick() {
-    console.log("Clicou no GitHub");
+  const [aboutMe, setAboutMe] = useState(false);
+
+  function handleAboutMe() {
+    setAboutMe(!aboutMe);
+  }
+
+  const [projects, setProjects] = useState(false);
+
+  function handleProjects() {
+    setProjects(!projects);
+  }
+
+  const [talkToMe, setTalkToMe] = useState(false);
+
+  function handletalkToMe() {
+    setTalkToMe(!talkToMe);
   }
 
   return (
     <StyledMenuContainer style={{ display: isOpen ? "block" : "none" }}>
       <StyledMenuList>
-        <li>_sobre-mim</li>
-        <li>_projetos</li>
-        <li>_fale-comigo</li>
-        <li>
+        <li onClick={() => handleAboutMe()}>
+          _sobre-mim 
+        </li>
+        {aboutMe && <>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam veritatis eos exercitationem sapiente veniam delectus animi sed, placeat id magnam explicabo? Quaerat esse quasi molestiae quia sit reiciendis explicabo odit.</>}
+        <li onClick={() => handleProjects()}>_projetos</li>
+        {projects && <>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam veritatis eos exercitationem sapiente veniam delectus animi sed, placeat id magnam explicabo? Quaerat esse quasi molestiae quia sit reiciendis explicabo odit.</>}
+        <li onClick={() => handletalkToMe()}>_fale-comigo</li>
+        {talkToMe && <>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam veritatis eos exercitationem sapiente veniam delectus animi sed, placeat id magnam explicabo? Quaerat esse quasi molestiae quia sit reiciendis explicabo odit.</>}
+        <li >
           _me-encontre-em:
           <a href="https://github.com/brunopassos" target="_blank">
             <AiFillGithub />
